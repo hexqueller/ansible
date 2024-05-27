922b3ae200d5c2416a69c715203e7e30354ddf09
V1RUSnya <dima.nesed@gmail.com>
2024-05-27 19:40:11 +0300

    Последняя буква в ELK - L- Logstash

--- prepare.sh
+++ prepare.sh
@@ -9,6 +9,9 @@ centos_file="vagrant/CentOS-7-x86_64-Vagrant-2004_01.VirtualBox.box"
 elk_url="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${elk_ver}-x86_64.rpm"
 elk_file="roles/elk/files/elasticsearch-${elk_ver}-x86_64.rpm"
 
+logstash_url="https://artifacts.elastic.co/downloads/logstash/logstash-${elk_ver}-x86_64.rpm"
+logstash_file="roles/elk/files/logstash-${elk_ver}-x86_64.rpm"
+
 kibana_url="https://artifacts.elastic.co/downloads/kibana/kibana-${elk_ver}-x86_64.rpm"
 kibana_file="roles/elk/files/kibana-${elk_ver}-x86_64.rpm"
 
@@ -19,5 +22,6 @@ mkdir -p $(dirname $centos_file) $(dirname $elk_file) $(dirname $kibana_file) $(
 
 wget -c $centos_url -O $centos_file
 wget -c $elk_url -O $elk_file
+wget -c $logstash_url -O $logstash_file
 wget -c $kibana_url -O $kibana_file
 wget -c $grafana_url -O $grafana_file
